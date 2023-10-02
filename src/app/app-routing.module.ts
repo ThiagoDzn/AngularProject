403759682821d58d8ad4879ce6aaa55a6751a0da
ThiagoDzn <thiagocruzdasilva941@gmail.com>
2023-10-02 20:26:53 -0300

    removendo comentarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { FooterComponent } from './meuscomponentes/footer/footer.component';
 import { LoginComponent } from './meuscomponentes/login/login.component';
 
 const routes: Routes = [
-  { path: 'header', component: HeaderComponent }, // Rota para o HeaderComponent
-  { path: 'footer', component: FooterComponent }, // Rota para o FooterComponent
+  { path: 'header', component: HeaderComponent }, 
+  { path: 'footer', component: FooterComponent }, 
   { path: 'login', component: LoginComponent }, // Rota para o LoginComponent
-  { path: '', redirectTo: '/login', pathMatch: 'full' } // Rota padrão para o LoginComponent
+  { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
 
 @NgModule({
